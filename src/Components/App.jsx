f3cfa2b901b023994b1d3b8dd60ed09d5758f211
Neeraj <asaini7788@gmail.com>
2021-01-06 13:01:50 -0500

    Default Notes Added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import Notes from "../notes"
+
+
+
 function App()
 {
-    return <div><Header /> <Note /><Footer /></div>
+    return <div><Header /> 
+    {Notes.map(noteItem =>(
+         <Note 
+         key = {noteItem.key}
+         title = {noteItem.title}
+         content = {noteItem.content} />
+    ))}
+    
+    <Footer /></div>
 }
 
 
 
 export default App;
+
+
+
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 
-function Note(){
-    return <div className = "note"><h1>Note Title</h1> <p> Note Conetnet</p></div>
+function Note(props){
+    return <div className = "note"><h1>{props.title}</h1> <p>{props.content}</p></div>
 }
 
 
